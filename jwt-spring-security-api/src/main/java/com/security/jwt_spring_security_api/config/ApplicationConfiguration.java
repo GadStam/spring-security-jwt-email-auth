8b128819591b28c2de54008a20ac743be3eb05b7
gadstamati <gadstam71@gmail.com>
2024-08-22 20:49:03 -0300

    jwt service done

---
+++ jwt-spring-security-api/src/main/java/com/security/jwt_spring_security_api/config/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package com.security.jwt_spring_security_api.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class ApplicationConfiguration {
+
+}
--- jwt-spring-security-api/src/main/java/com/security/jwt_spring_security_api/repository/UserRepository.java
+++ jwt-spring-security-api/src/main/java/com/security/jwt_spring_security_api/repository/UserRepository.java
@@ -1,9 +1,19 @@
 package com.security.jwt_spring_security_api.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import com.security.jwt_spring_security_api.model.User;
 
 @Repository
 
-public interface UserRepository {
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationCode(String verificationCode);
+    
+
+    
 
 }
