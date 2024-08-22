e0524fa82f936a2cc1316c3a1f084bfe3707c67b
gadstamati <gadstam71@gmail.com>
2024-08-22 14:26:23 -0300

    creating repository

--- jwt-spring-security-api/src/main/java/com/security/jwt_spring_security_api/model/User.java
+++ jwt-spring-security-api/src/main/java/com/security/jwt_spring_security_api/model/User.java
@@ -1,10 +1,16 @@
 package com.security.jwt_spring_security_api.model;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
@@ -26,15 +32,49 @@ public class User implements UserDetails{
     @Column(nullable = false)
     private String password;
 
+    private boolean enabled;
+
+    @Column(name = "verification_code")
+    private String verificationCode;
+
+    @Column(name = "verification_expiration")
+    private LocalDateTime verificationExpirationAt;
+
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
     }
 
-    public User(Long id) {
-        this.id = id;
+    public User() {
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+
 
     
 
---
+++ jwt-spring-security-api/src/main/java/com/security/jwt_spring_security_api/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.security.jwt_spring_security_api.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface UserRepository {
+
+}
